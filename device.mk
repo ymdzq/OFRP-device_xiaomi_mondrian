a1a61e74a2849cb6d2967b3c8c8cb335600f698f
ymdzq <233900743@qq.com>
2023-06-04 22:28:24 +0800

    调整device.mk 设置动态分区标记 设置fastbootd标记 设置api版本号 设置vab标记 删除部分重复packages

--- device.mk
+++ device.mk
@@ -6,30 +6,25 @@
 #
 
 LOCAL_PATH := device/xiaomi/mondrian
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+# API
+PRODUCT_TARGET_VNDK_VERSION := 33
+PRODUCT_SHIPPING_API_LEVEL := 31
+
 # A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+ENABLE_VIRTUAL_AB := true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.taro
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.taro \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
