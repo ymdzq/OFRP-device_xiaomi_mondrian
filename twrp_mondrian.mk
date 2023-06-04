329ffdbbc89a641ce0bc076168b6d88ca2e5aa76
ymdzq <233900743@qq.com>
2023-06-04 22:33:57 +0800

    设置厂商和型号 添加core_minimal和vab_ota配置文件

--- twrp_mondrian.mk
+++ twrp_mondrian.mk
@@ -5,9 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := mondrian
+
 # Inherit from those products. Most specific first.
+# 引用默认的编译配置文件，core_64_bit必须先于其他引用，否则不生效
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -19,7 +26,8 @@ PRODUCT_DEVICE := mondrian
 PRODUCT_NAME := twrp_mondrian
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23013RK75C
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+#PRODUCT_RELEASE_NAME := Redmi K60
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
