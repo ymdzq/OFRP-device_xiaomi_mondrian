fbcb85388c25a725ccbc4d5c7584e780d6cf3903
ymdzq <1422091+ymdzq@users.noreply.github.com>
2023-06-23 19:57:20 +0800

    默认关闭夏令时 twrp默认打开夏令时，会导致默认时间快一小时

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -117,6 +117,8 @@
 	export OF_UNBIND_SDCARD_F2FS=1
 	# 设置默认时区为北京时间
 	export OF_DEFAULT_TIMEZONE="TAIST-8;TAIDT"
+	# 设置默认关闭夏令时
+	export TW_TIME_ZONE_GUIDST=0
 
 	F=$(find "device" -name "mondrian")
 	# 修改启动画面背景色为#000000
