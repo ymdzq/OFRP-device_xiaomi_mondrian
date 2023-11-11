adc4480797ae1d74b0ac49e6639e554c158877db
ymdzq <233900743@qq.com>
2023-11-11 19:59:53 +0800

    添加这个机器支持新的禁用AVB2.0功能的标记

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -117,6 +117,8 @@
 	export OF_UNBIND_SDCARD_F2FS=1
 	# 设置默认时区为北京时间
 	export OF_DEFAULT_TIMEZONE="TAIST-8;TAIDT"
+	# 支持通过修补vbmeta/vbmeta_system禁用avb2.0
+	export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
 
 	F=$(find "device" -maxdepth 2 -name "mondrian")
 	# 修改启动画面背景色为#000000
