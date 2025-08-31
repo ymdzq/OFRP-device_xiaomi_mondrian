88115649b2b677e9cd602ae7569bf608bb5c712e
ymdzq <ymdzq@users.noreply.github.com>
2025-08-31 11:52:25 +0800

    指定默认keymaster版本为4.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -129,6 +129,8 @@
 	export OF_OPTIONS_LIST_NUM=11
 	# 跳过新版fstab处理
 	export OF_FORCE_USE_RECOVERY_FSTAB=1
+	# 指定默认keymaster版本为4.1
+	export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
 	F=$(find "device" -maxdepth 2 -name "mondrian")
 	# 修改启动画面背景色为#000000
